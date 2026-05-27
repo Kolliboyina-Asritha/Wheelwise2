8d462947a034110aebf184d9a3d6fc96c5179e1c
Kolliboyina-Asritha <asrithaasritha99@gmail.com>
2026-05-27 20:54:22 +0530

    Update logoutController.js

--- logcontroller/logoutController.js
+++ logcontroller/logoutController.js
@@ -8,14 +8,14 @@ const handleLogout= async (req,res)=>{
     //is refreshtoken in DB
     const foundUser=await User.findOne({refreshToken}).exec();
     if(!foundUser) {
-        res.clearCookie('jwt',{httpOnly:true,sameSite:'None'});
+        res.clearCookie('jwt',{httpOnly:true,secure:true,sameSite:'None'});
         return res.sendStatus(204);//forbidden
     }
     //delete the refresh token in DB
     foundUser.refreshToken='';
     const result=await foundUser.save();
     console.log(result);
-    res.clearCookie('jwt',{httpOnly:true,sameSite:'None'});
+    res.clearCookie('jwt',{httpOnly:true,secure:true,sameSite:'None'});
     res.sendStatus(204);
  
 };
