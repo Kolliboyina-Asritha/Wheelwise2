11a11c856d191b11ab2ba7fb9dee1cb7a61558a9
Kolliboyina-Asritha <asrithaasritha99@gmail.com>
2026-05-27 22:20:38 +0530

    Update mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -9,15 +9,5 @@ const transporter = nodemailer.createTransport({
 });
 
 module.exports = transporter;
-transporter.sendMail({
-  from: process.env.EMAIL_USER,
-  to: process.env.EMAIL_USER,
-  subject: 'Test Email',
-  html: '<p>This is a test email</p>',
-}, (err, info) => {
-  if (err) {
-    console.error('❌ Email failed:', err);
-  } else {
-    console.log('✅ Email sent:', info.response);
-  }
+
 });
