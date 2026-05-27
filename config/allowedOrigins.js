147ae8bebca04b219df6dc6fcd42258d008d78b0
Kolliboyina-Asritha <asrithaasritha99@gmail.com>
2026-05-27 20:52:19 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,2 +1,2 @@
-const allowedOrigins=['https://spontaneous-zuccutto-d8156a.netlify.app','http://127.0.0.1:5500','http://localhost:6006','http://localhost:3000'];
+const allowedOrigins=['https://spontaneous-zuccutto-d8156a.netlify.app','http://127.0.0.1:5500','http://localhost:6006','http://localhost:3000','https://buyurdreamvehicle.netlify.app'];
 module.exports=allowedOrigins;
